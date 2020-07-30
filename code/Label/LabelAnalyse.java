e1ec8c788fc0a8f3fef4957da7012422795a653c
xp2020rlby <xp2020rlby@.github.com>
2020-07-30 12:01:00 +0800

    删除注释代码

--- code/Label/LabelAnalyse.java
+++ code/Label/LabelAnalyse.java
@@ -18,26 +18,18 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 class LabelAnaMapper extends Mapper<Text, Text, Text, Text> {
-    //private Text rkey = new Text();
-    //private Text rvalue = new Text();
     @Override
     protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
         String name = key.toString();//名字
         String list = value.toString();//value （标签+邻接表）
         String label = list.split("#")[0];//标签
-
         String relationlist = "#"+list.split("#")[1];//#+邻接表
-        //rkey.set(name);
-        //rvalue.set(relations);
         context.write(new Text(name), new Text(relationlist));//发送邻接表维护网络结构
         String[] slist = relationlist.split("\\[|\\]")[1].split("\\|");//将每个关系分离
 
         for (String s : slist) {
             String target = s.split(",")[0];//和名字主人产生影响的人名
             String weight = s.split(",")[1];//权值
-            //keyInfo.set(target);
-            //valueInfo.set(label + "|" + weight);
-            //context.write(keyInfo, valueInfo);
             String LabelWeight=label+":"+weight;
             context.write(new Text(target), new Text(LabelWeight));
         }
@@ -48,11 +40,9 @@ class LabelAnaReducer extends Reducer<Text, Text, Text, Text> {
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException,
             InterruptedException {
-        //String name = key.toString();
         HashMap<String, Double> neighborMap = new HashMap<>();
         String list = "";
         for (Text value : values) {
-            //context.write(key, t);
             String tempstr = value.toString();
             if (tempstr.startsWith("#")) {
                 list = tempstr.substring(1);//从#后面截取整个邻接表
@@ -67,7 +57,6 @@ class LabelAnaReducer extends Reducer<Text, Text, Text, Text> {
                 }
             }
         }
-
         String label = "";
         Double maxWeight = 0.0;
         for (Map.Entry<String, Double> entry : neighborMap.entrySet()) {
@@ -78,9 +67,6 @@ class LabelAnaReducer extends Reducer<Text, Text, Text, Text> {
                 label = keylable;
             }
         }
-
-
-
         String reslist = label + "#" + list;
         context.write(key, new Text(reslist));
 
@@ -112,17 +98,13 @@ public class LabelAnalyse {
             job.setOutputValueClass(Text.class);
             job.setInputFormatClass(KeyValueTextInputFormat.class);// 以/t切割key与value
             job.setNumReduceTasks(5);//5个reducer
-            //job.getConfiguration().set("labeled", labeled);
-
             FileInputFormat.addInputPath(job, new Path(input));
-
             Path path = new Path(output);
             FileSystem fileSystem = path.getFileSystem(conf);// 根据path找到这个文件
             if (fileSystem.exists(path)) {
-                fileSystem.delete(path, true);// 设置true，就算output有东西，也一带删除
+                fileSystem.delete(path, true);// 设置true
             }
             FileOutputFormat.setOutputPath(job, new Path(output));
-
             job.waitForCompletion(true);
             input = output;
         }
